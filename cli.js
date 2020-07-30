55e3613f274e2932eaac93ddea5605ec4ebda16a
Gary Chew <garylchew@gmail.com>
2020-07-29 23:48:47 -0700

    Create skeleton cli.js is-modern.js

---
+++ cli.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+'use strict'
+
+const isModern = require("./src/is-modern");
+
+console.log(isModern());
---
+++ src/is-modern.js
@@ -0,0 +1,7 @@
+'use strict'
+
+function isModern(code) {
+    return false;
+}
+
+module.exports = isModern;
