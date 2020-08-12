c1a70c4ee7a3137e556dfdc52c66f5d8926da501
Gary Chew <garylchew@gmail.com>
2020-08-11 22:36:32 -0700

    Implement CLI

--- cli.js
+++ cli.js
@@ -1,4 +1,25 @@
 #!/usr/bin/env node
 'use strict'
+const ora = require("ora");
+const { getPackageEcmaVersion, isEcmaVersionModern } = require("./src/index.js");
 
-const { getEcmaVersion } = require("./src/index.js");
+function main(packageString) {
+    const spinner = ora({stream: process.stdout});
+    spinner.text = `Fetching ${packageString}`;
+    spinner.start();
+    try {
+        const ecmaVersion = getPackageEcmaVersion(packageString)
+        if (isEcmaVersionModern(ecmaVersion)) {
+            spinner.succeed(`${packageString} is modern (ES${ecmaVersion})!`);
+        } else {
+            spinner.warn(`${packageString} is legacy (ES${ecmaVersion}).`);
+        }
+        process.exit(0);
+    } catch (e) {
+        spinner.fail(`Unable to fetch the package ${packageString}. ${e.message}`);
+        process.exit(1);
+    }
+}
+
+const packageString = process.argv[2];
+main(packageString);
