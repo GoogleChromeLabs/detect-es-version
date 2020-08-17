083559832e8e1d0f365cf99a379ddccc27e4a453
Gary Chew <garylchew@gmail.com>
2020-08-17 06:30:05 -0700

    Change enhanced-resolve to promisified version

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 'use strict'
 const fs = require('fs');
+const { promisify } = require('util');
 const parsePackageString = require('npm-package-arg');
 const enhancedResolve = require('enhanced-resolve');
 const InstallationUtils = require('./utils/installation-utils');
 const getEcmaVersionAST = require('./get-ecma-version-ast.js');
 
-const resolve = enhancedResolve.create.sync({
-    modules: ['node_modules'],
-    mainFields: ['browser', 'module', 'main', 'style'],
-});
+const resolve = promisify(enhancedResolve.create({
+    mainFields: ['module', 'browser', 'main'],
+}));
 
 function isEcmaVersionModern(ecmaVersion) {
     return ecmaVersion > 5;
 }
 
 async function getEntryPointEcmaVersion(context, packageName) {
-    const resolvedPath = resolve(context, packageName);
+    const resolvedPath = await resolve(context, packageName);
     const code = fs.readFileSync(resolvedPath, 'utf8');
     // TODO (ISSUE#5) Implement recursive getEntryPointEcmaVersion
     return getEcmaVersion(code);
@@ -25,8 +25,8 @@ async function getPackageEcmaVersion(packageString) {
     const parsedPackage = parsePackageString(packageString);
     const installPath = await InstallationUtils.getInstallPath();
     await InstallationUtils.installPackage(parsedPackage.name, installPath);
-    const ecmaVersion = getEntryPointEcmaVersion(installPath, parsedPackage.name);
-    InstallationUtils.cleanupPath(installPath);
+    const ecmaVersion = await getEntryPointEcmaVersion(installPath, parsedPackage.name);
+    await InstallationUtils.cleanupPath(installPath);
     return ecmaVersion;
 }
 
