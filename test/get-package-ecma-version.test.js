d818d022897ecd778f2310b4c0dc604de63125c2
Gary Chew <garylchew@gmail.com>
2020-08-21 10:52:03 -0700

    Create tests and benchmarks for getPackageEcmaVersion

--- test/get-package-ecma-version.test.js
+++ test/get-package-ecma-version.test.js
@@ -1,15 +1,22 @@
 const { getPackageEcmaVersion } = require('../src/index.js');
 
-it.each`
-  packageName         | expected
-  ${'request@2.88.2'} | ${5}
-  ${'react@16.13.1'}  | ${5}
-  ${'lit-html@1.2.1'} | ${2015}
-  ${'lodash@4.17.19'} | ${5}
-  ${'conf@7.1.1'}     | ${2015}
-`(
-  'getPackageEcmaVersion should return $expected for $packageName',
-  async ({ packageName, expected }) => {
+describe('getPackageEcmaVersion should', () => {
+  it.each`
+    packageName                          | expected
+    ${'bowser@2.10.0'}                   | ${2015}
+    ${'chalk@4.1.0'}                     | ${2015}
+    ${'@lattice/configurer@0.0.1'}       | ${5}
+    ${'@babel/core@7.11.4'}              | ${2015}
+    ${'request@2.88.2'}                  | ${5}
+    ${'react@16.13.1'}                   | ${5}
+    ${'lit-html@1.2.1'}                  | ${2015}
+    ${'lodash@4.17.19'}                  | ${5}
+    ${'lodash-es@4.17.15'}               | ${5}
+    ${'conf@7.1.1'}                      | ${2015}
+    ${'swr@0.3.0'}                       | ${5}
+    ${'vue@2.6.11'}                      | ${5}
+    ${'@nelsongomes/ts-timeframe@0.2.2'} | ${5}
+  `('return $expected for $packageName', async ({ packageName, expected }) => {
     expect(await getPackageEcmaVersion(packageName)).toBe(expected);
-  }
-);
+  });
+});
